29b20918c1c0baae51df787a2faf3eae53bafb5c
Ben Lewis <blewis@theparkingspot.com>
2019-10-08 16:24:38 -0500

    Initial Commit

---
+++ On the Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On the Map
+//
+//  Created by Ben Lewis on 10/8/19.
+//  Copyright © 2019 Ben Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
